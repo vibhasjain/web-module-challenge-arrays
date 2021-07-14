afd0e92ac6fefd30ac27b1d06e86f356d135b04e
vibes <vibes@vibess-MacBook-Pro.local>
2021-07-14 13:42:40 -0400

    lets go

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
+function filterByWord(){
   /*your code here*/
 }
 
